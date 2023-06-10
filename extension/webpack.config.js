9506459017a0aba12fa949e41f5c38f2ac84195c
Hassan <hassanzerouali@Hassans-MacBook-Pro.local>
2023-06-10 18:17:12 +0200

    fixing small errors & adding npm start

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 module.exports = {
     entry: {
-        content: path.resolve(__dirname,"..","react-app/src/extensionExports/content/content-script-index.js"),
-        background:  path.resolve(__dirname,"..","react-app/src/extensionExports/background/background-script-index.js"),
-        popup: path.resolve(__dirname,"..","react-app/src/extensionExports/popup/popup-script-index.js"),
-        displayPage:path.resolve(__dirname,"..","react-app/src/extensionExports/page/page-script-index.js")
+        content: path.resolve(__dirname,"..","components-builder/src/extensionExports/content/content-script-index.js"),
+        background:  path.resolve(__dirname,"..","components-builder/src/extensionExports/background/background-script-index.js"),
+        popup: path.resolve(__dirname,"..","components-builder/src/extensionExports/popup/popup-script-index.js"),
+        displayPage:path.resolve(__dirname,"..","components-builder/src/extensionExports/page/page-script-index.js")
         
     },
     output: {
@@ -34,21 +34,21 @@ module.exports = {
     plugins: [new CopyPlugin({
         patterns: [
             {
-                from: "./src/popup",
+                from: "./popup",
                 to: ".",
                 globOptions: {
                     ignore: ["**/*.js"],
                 },
             },
             {
-                from: "./src/page",
+                from: "./page",
                 globOptions: {
                     ignore: ["**/*.js"],
                 },
                 to: "."
             },
             {
-                from: "./src/assets",
+                from: "./assets",
                 to: "."
             }
         ]
